a448b9bcbef20eeaaebac22ec1a8700e8a7c98ae
meeeejin <meeeeejin@gmail.com>
2019-09-19 00:09:58 +0900

    Update wasd.go

--- wasd.go
+++ wasd.go
@@ -1,14 +1,14 @@
 package main
 
 import (
+	"github.com/faiface/pixel/pixelgl"
 	"github.com/h8gi/canvas"
 	"golang.org/x/image/colornames"
-	"golang.org/x/mobile/event/key"
 )
 
 // WASD로 도형 이동
 func main() {
-	c := canvas.New(&canvas.NewCanvasOptions{
+	c := canvas.NewCanvas(&canvas.CanvasConfig{
 		Width:     1200,
 		Height:    1200,
 		FrameRate: 60,
@@ -28,17 +28,14 @@ func main() {
 		ctx.Clear()
 		ctx.SetColor(colornames.Red)
 
-		if ctx.KeyPressed() {
-			e := ctx.KeyEvent()
-			if e.Code == key.CodeW {
-				me.Y -= 10
-			} else if e.Code == key.CodeA {
-				me.X -= 10
-			} else if e.Code == key.CodeS {
-				me.Y += 10
-			} else if e.Code == key.CodeD {
-				me.X += 10
-			}
+		if ctx.IsKeyPressed(pixelgl.KeyW) {
+			me.Y += 10
+		} else if ctx.IsKeyPressed(pixelgl.KeyA) {
+			me.X -= 10
+		} else if ctx.IsKeyPressed(pixelgl.KeyS) {
+			me.Y -= 10
+		} else if ctx.IsKeyPressed(pixelgl.KeyD) {
+			me.X += 10
 		}
 
 		ctx.DrawRectangle(me.X-50, me.Y-50, 100, 100)
